b6ef932ea02cd6dafb283ac34f218f0db541acf0
quangthanghust <thang.tq207701@sis.hust.edu.vn>
2023-11-29 22:02:24 +0700

    Code con quai 1

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -1,45 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class EnemyMovement : MonoBehaviour
 {
 
     [Header("References")]
     [SerializeField] private Rigidbody2D rb;
-    
+
     [Header("Attributes")]
-    [SerializeField]
-    private float moveSpeed = 2f;
+    [SerializeField] private float moveSpeed = 2f;
 
     private Transform target;
-    
-    
+    private int pathIndex = 0;
 
-    // Use this for initialization
+
     private void Start()
     {
         target = LevelManager.main.path[pathIndex];
     }
 
-    // Update is called once per frame
     private void Update()
     {
-        if (Vector2.Distance(target.position,transform.position) <= 0.1f){
+        if (Vector2.Distance(target.position, transform.position) <= 0.1f)
+        {
             pathIndex++;
-            if (pathIndex == LevelManager.main.path.Length){
+
+            if (pathIndex == LevelManager.main.path.Length)
+            {
                 Destroy(gameObject);
                 return;
             }
-            else{
+            else
+            {
                 target = LevelManager.main.path[pathIndex];
             }
         }
     }
 
-    private void FixedUpdate(){
-        Vector2 direction =(target.position - transform.position).normalized;
+    private void FixedUpdate()
+    {
+        Vector2 direction = (target.position - transform.position).normalized;
+
         rb.velocity = direction * moveSpeed;
     }
+
 
 }
