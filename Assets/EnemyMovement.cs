a27f79f673bca255a4772056d9946a59e56305d6
NamAnhNguyen2803 <namanh8t@gmail.com>
2023-11-30 12:57:28 +0700

    30/11

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -1,45 +1,74 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class EnemyMovement : MonoBehaviour
 {
 
     [Header("References")]
     [SerializeField] private Rigidbody2D rb;
-    
+
     [Header("Attributes")]
-    [SerializeField]
-    private float moveSpeed = 2f;
+    [SerializeField] private float moveSpeed = 2f;
 
     private Transform target;
-    
-    
+    private int pathIndex = 0;
 
-    // Use this for initialization
+
     private void Start()
     {
+        // Khởi tạo rb nếu nó chưa được khởi tạo
+        if (rb == null)
+        {
+            rb = GetComponent<Rigidbody2D>();
+
+            // Kiểm tra xem rb đã được khởi tạo thành công hay không
+            if (rb == null)
+            {
+                Debug.LogError("Rigidbody2D is not initialized!");
+            }
+        }
+
         target = LevelManager.main.path[pathIndex];
     }
 
-    // Update is called once per frame
+
+
     private void Update()
     {
-        if (Vector2.Distance(target.position,transform.position) <= 0.1f){
+        // Kiểm tra xem target đã được khởi tạo chưa
+        if (target != null && Vector2.Distance(target.position, transform.position) <= 0.1f)
+        {
             pathIndex++;
-            if (pathIndex == LevelManager.main.path.Length){
+
+            if (pathIndex == LevelManager.main.path.Length)
+            {
                 Destroy(gameObject);
                 return;
             }
-            else{
+            else
+            {
                 target = LevelManager.main.path[pathIndex];
             }
         }
     }
 
-    private void FixedUpdate(){
-        Vector2 direction =(target.position - transform.position).normalized;
-        rb.velocity = direction * moveSpeed;
+
+    private void FixedUpdate()
+    {
+        // Kiểm tra xem rb đã được khởi tạo chưa
+        if (rb != null)
+        {
+            Vector2 direction = (target.position - transform.position).normalized;
+            rb.velocity = direction * moveSpeed;
+        }
+        else
+        {
+            Debug.LogError("Rigidbody2D is null in FixedUpdate!");
+        }
     }
+
 
 }
--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -1,52 +1,52 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public class EnemySpawner : MonoBehaviour
-// {
-//     [Header("References")]
-//     [SerializeField]  private GameObject[] enemyPrefabs; 
+public class EnemySpawner : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField]  private GameObject[] enemyPrefabs; 
     
-//     [Header("Attributes")]
-//     [SerializeField] private int baseEnemies = 8;
-//     [SerializeField] private float enemiesPerSecond = 0.5f;
-//     [SerializeField] private float timeBetweenWaves = 5f;
-//     [SerializeField] private float difficultyScalingFactor = 0.75f;
-//     private int currentWave = 1;
-//     private float timeSinceLastSpawn;
-//     private int enemiesAlive;
-//     private int enemiesLeftToSpawn;
-//     private bool isSpawning = false;
-//     // Start is called before the first frame update
-//     void Start()
-//     {   
-//         StartWave();
-//     }
+    [Header("Attributes")]
+    [SerializeField] private int baseEnemies = 8;
+    [SerializeField] private float enemiesPerSecond = 0.5f;
+    [SerializeField] private float timeBetweenWaves = 5f;
+    [SerializeField] private float difficultyScalingFactor = 0.75f;
+    private int currentWave = 1;
+    private float timeSinceLastSpawn;
+    private int enemiesAlive;
+    private int enemiesLeftToSpawn;
+    private bool isSpawning = false;
+    // Start is called before the first frame update
+    void Start()
+    {   
+        StartWave();
+    }
 
-//     // Update is called once per frame
-//     private void Update()
-//     {
-//         if (!isSpawning) return;
-//         timeSinceLastSpawn += Time.deltaTime;
+    // Update is called once per frame
+    private void Update()
+    {
+        if (!isSpawning) return;
+        timeSinceLastSpawn += Time.deltaTime;
 
-//         if (timeSinceLastSpawn >= (1f / enemiesPerSecond) && enemiesLeftToSpawn > 0) {
-//             SpawnEnemy();
-//             enemiesLeftToSpawn--;
-//             enemiesAlive++;
-//             timeSinceLastSpawn = 0f;
-//         }   
-//     }
-//     private void StartWave(){
-//         isSpawning = true;
-//         enemiesLeftToSpawn = EnemiesPerWave();
-//     }
+        if (timeSinceLastSpawn >= (1f / enemiesPerSecond) && enemiesLeftToSpawn > 0) {
+            SpawnEnemy();
+            enemiesLeftToSpawn--;
+            enemiesAlive++;
+            timeSinceLastSpawn = 0f;
+        }   
+    }
+    private void StartWave(){
+        isSpawning = true;
+        enemiesLeftToSpawn = EnemiesPerWave();
+    }
 
-//     private void SpawnEnemy(){
-//         //Debug.Log("Spawn Enemy");
-//         GameObject prefabToSpawn = enemyPrefabs[0];
-//         Instantiate(prefabToSpawn, LevelManager.main.startPoint.position,Quaternion.identity);
-//     }
-//     private int EnemiesPerWave(){
-//         return Mathf.RoundToInt(baseEnemies * Mathf.Pow(currentWave , difficultyScalingFactor));
-//     }
-// }
+    private void SpawnEnemy(){
+        //Debug.Log("Spawn Enemy");
+        GameObject prefabToSpawn = enemyPrefabs[0];
+        Instantiate(prefabToSpawn, LevelManager.main.startPoint.position,Quaternion.identity);
+    }
+    private int EnemiesPerWave(){
+        return Mathf.RoundToInt(baseEnemies * Mathf.Pow(currentWave , difficultyScalingFactor));
+    }
+}
