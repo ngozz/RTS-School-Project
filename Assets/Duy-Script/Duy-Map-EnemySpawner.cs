70bb82c35561b44ff076ad9829d52a5f3501ebcf
Nguyễn Việt Hòa <84431330+Hoanguyen-2002@users.noreply.github.com>
2023-12-04 09:51:26 +0700

    comment code and add new scene

--- Assets/Duy-Script/Duy-Map-EnemySpawner.cs
+++ Assets/Duy-Script/Duy-Map-EnemySpawner.cs
@@ -1,88 +1,88 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.InteropServices;
-using UnityEngine;
-using UnityEngine.Events;
-using UnityEngine.EventSystems;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Runtime.InteropServices;
+//using UnityEngine;
+//using UnityEngine.Events;
+//using UnityEngine.EventSystems;
 
-public class EnemySpawner : MonoBehaviour
-{
-    [Header("References")]
-    [SerializeField] private GameObject[] enemyPrefabs;
+//public class EnemySpawner : MonoBehaviour
+//{
+//    [Header("References")]
+//    [SerializeField] private GameObject[] enemyPrefabs;
 
-    [Header("Attributes")]
-    [SerializeField] private int baseEnemies = 8;
-    [SerializeField] private float enemiesPerSecond = 0.5f;
-    [SerializeField] private float timeBetweenWaves = 5f;
-    [SerializeField] private float difficultyScalingFactor = 0.75f;
+//    [Header("Attributes")]
+//    [SerializeField] private int baseEnemies = 8;
+//    [SerializeField] private float enemiesPerSecond = 0.5f;
+//    [SerializeField] private float timeBetweenWaves = 5f;
+//    [SerializeField] private float difficultyScalingFactor = 0.75f;
 
-    [Header("Events")]
-    public static UnityEvent onEnemyDestroy = new UnityEvent();
+//    [Header("Events")]
+//    public static UnityEvent onEnemyDestroy = new UnityEvent();
 
-    private int currentWave = 1;
-    private float timeSinceLastSpawn;
-    private int enemiesAlive;
-    private int enemiesLeftToSpawn;
-    private bool isSpawning = false;
+//    private int currentWave = 1;
+//    private float timeSinceLastSpawn;
+//    private int enemiesAlive;
+//    private int enemiesLeftToSpawn;
+//    private bool isSpawning = false;
 
-    private void Awake()
-    {
-        onEnemyDestroy.AddListener(EnemyDestroyed);
-    }
+//    private void Awake()
+//    {
+//        onEnemyDestroy.AddListener(EnemyDestroyed);
+//    }
 
-    private void Start()
-    {
-        StartCoroutine(StartWave());
-    }
+//    private void Start()
+//    {
+//        StartCoroutine(StartWave());
+//    }
 
-    private void Update()
-    {
-        if (!isSpawning) { return; }
-        timeSinceLastSpawn += Time.deltaTime;
+//    private void Update()
+//    {
+//        if (!isSpawning) { return; }
+//        timeSinceLastSpawn += Time.deltaTime;
 
-        if (timeSinceLastSpawn >= (1f / enemiesPerSecond) && enemiesLeftToSpawn > 0)
-        {
-            SpawnEnemy();
-            enemiesLeftToSpawn--;
-            enemiesAlive++;
-            timeSinceLastSpawn = 0f;
-        }
+//        if (timeSinceLastSpawn >= (1f / enemiesPerSecond) && enemiesLeftToSpawn > 0)
+//        {
+//            SpawnEnemy();
+//            enemiesLeftToSpawn--;
+//            enemiesAlive++;
+//            timeSinceLastSpawn = 0f;
+//        }
 
-        if (enemiesAlive == 0 && enemiesLeftToSpawn == 0)
-        {
-            EndWave();
-        }
+//        if (enemiesAlive == 0 && enemiesLeftToSpawn == 0)
+//        {
+//            EndWave();
+//        }
 
-    }
+//    }
 
-    private void EnemyDestroyed()
-    {
-        enemiesAlive--;
-    }
+//    private void EnemyDestroyed()
+//    {
+//        enemiesAlive--;
+//    }
 
-    private IEnumerator StartWave()
-    {
-        yield return new WaitForSeconds(timeBetweenWaves);
-        isSpawning = true;
-        enemiesLeftToSpawn = EnemiesPerWave();
-    }
+//    private IEnumerator StartWave()
+//    {
+//        yield return new WaitForSeconds(timeBetweenWaves);
+//        isSpawning = true;
+//        enemiesLeftToSpawn = EnemiesPerWave();
+//    }
 
-    private void EndWave()
-    {
-        isSpawning = false;
-        timeSinceLastSpawn = 0f;
-        currentWave++;
-        StartCoroutine(StartWave());
-    }
+//    private void EndWave()
+//    {
+//        isSpawning = false;
+//        timeSinceLastSpawn = 0f;
+//        currentWave++;
+//        StartCoroutine(StartWave());
+//    }
 
-    private void SpawnEnemy()
-    {
-        GameObject prefabsToSpawn = enemyPrefabs[0];
-        Instantiate(prefabsToSpawn, LevelManager.main.startPoint.position, Quaternion.identity);
-    }
+//    private void SpawnEnemy()
+//    {
+//        GameObject prefabsToSpawn = enemyPrefabs[0];
+//        Instantiate(prefabsToSpawn, LevelManager.main.startPoint.position, Quaternion.identity);
+//    }
 
-    private int EnemiesPerWave()
-    {
-        return Mathf.RoundToInt(baseEnemies * Mathf.Pow(currentWave, difficultyScalingFactor));
-    }
-}
+//    private int EnemiesPerWave()
+//    {
+//        return Mathf.RoundToInt(baseEnemies * Mathf.Pow(currentWave, difficultyScalingFactor));
+//    }
+//}
--- Assets/Duy-Script/Duy-Map-LevelManager.cs
+++ Assets/Duy-Script/Duy-Map-LevelManager.cs
@@ -1,26 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class LevelManager : MonoBehaviour
-{
-    public static LevelManager main;
+//public class LevelManager : MonoBehaviour
+//{
+//    public static LevelManager main;
 
-    public Transform startPoint;
-    public Transform[] path;
-    private void Awake()
-    {
-        main = this;
-    }
-    /*// Start is called before the first frame update
-    void Start()
-    {
+//    public Transform startPoint;
+//    public Transform[] path;
+//    private void Awake()
+//    {
+//        main = this;
+//    }
+//    /*// Start is called before the first frame update
+//    void Start()
+//    {
         
-    }
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
+//    // Update is called once per frame
+//    void Update()
+//    {
         
-    }*/
-}
+//    }*/
+//}
