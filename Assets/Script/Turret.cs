54b43f0cce81ec54450fe667b76acb93482d2b56
Nguyễn Việt Hòa <84431330+Hoanguyen-2002@users.noreply.github.com>
2023-12-04 22:10:14 +0700

    rotate point fix

--- Assets/Script/Turret.cs
+++ Assets/Script/Turret.cs
@@ -48,7 +48,7 @@ public class Turret : MonoBehaviour
 
     private void RotateTowardTarget()
     {
-        float angle = Mathf.Atan2(target.position.y - transform.position.y, target.position.x - transform.position.x) * Mathf.Rad2Deg +90f;
+        float angle = Mathf.Atan2(target.position.y - transform.position.y, target.position.x - transform.position.x) * Mathf.Rad2Deg -90f;
 
         Quaternion targetRotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
         turretRotationPoint.rotation = Quaternion.RotateTowards(turretRotationPoint.rotation, targetRotation, rotationSpeed * Time.deltaTime);
