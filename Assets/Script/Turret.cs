16d1126ce233eb042a1447610e5725898174cd79
Hoanguyen-2002 <viethoah928@gmail.com>
2023-12-05 11:00:28 +0700

    update turret can destroy enemy

--- Assets/Script/Turret.cs
+++ Assets/Script/Turret.cs
@@ -8,12 +8,16 @@ public class Turret : MonoBehaviour
     [Header("References")]
     [SerializeField] private Transform turretRotationPoint;
     [SerializeField] private LayerMask enemyMask;
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform firingPoint;
 
     [Header("Attribute")]
     [SerializeField] private float targetingRange = 5f;
     [SerializeField] private float rotationSpeed = 200f;
+    [SerializeField] private float bps = 1f;//bullet per second
 
     private Transform target;
+    private float timeUntilFire;
 
     private void Update()
     {
@@ -28,7 +32,23 @@ public class Turret : MonoBehaviour
         if (!CheckTargetIsInRange())
         {
             target = null;
+        } else {
+            timeUntilFire += Time.deltaTime;
+
+            if (timeUntilFire >= 1f / bps)
+            {
+                Shoot();
+                timeUntilFire = 0f;
+            }
         }
+    }
+
+    private void Shoot()
+    {
+        GameObject bulletObj = Instantiate(bulletPrefab, firingPoint.position, Quaternion.identity);
+        Bullet bulletScript = bulletObj.GetComponent<Bullet>();
+        bulletScript.SetTarget(target);
+
     }
 
     private void FindTarget()
