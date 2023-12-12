f0553a2a669315b7e88aaddcb6b8489613e53aa3
Lê Duy Hiếu <hieu24052002@gmail.com>
2023-12-12 19:53:58 +0700

    fix turn

--- Assets/Script/Turn.cs
+++ Assets/Script/Turn.cs
@@ -4,38 +4,30 @@ public class Turn : MonoBehaviour
 {
     private bool facingRight = true;
     private float lastX;
+    private SpriteRenderer spriteRenderer;
 
     void Start()
     {
-        // Initialize lastX with the enemy's starting x position
         lastX = transform.position.x;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        facingRight = !spriteRenderer.flipX;
     }
 
     void Update()
     {
-        // Check if the enemy's x position has decreased (moving left)
-        if (transform.position.x < lastX && facingRight)
-        {
-            Flip();
-        }
-        // Check if the enemy's x position has increased (moving right)
-        else if (transform.position.x > lastX && !facingRight)
+        float currentX = transform.position.x;
+
+        if (currentX < lastX && facingRight || currentX > lastX && !facingRight)
         {
             Flip();
         }
 
-        // Update lastX with the enemy's current x position
-        lastX = transform.position.x;
+        lastX = currentX;
     }
 
     void Flip()
     {
-        // Switch the way the enemy is labelled as facing
         facingRight = !facingRight;
-
-        // Multiply the enemy's x local scale by -1
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
-        transform.localScale = theScale;
+        spriteRenderer.flipX = !spriteRenderer.flipX;
     }
 }
