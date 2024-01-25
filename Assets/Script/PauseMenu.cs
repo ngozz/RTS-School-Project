7b7e30c82bb8cc33caa8bd7105cb65b80b96e629
quangthanghust <thang.tq207701@sis.hust.edu.vn>
2024-01-26 01:02:49 +0700

    sửa lại chuyển scene

--- Assets/Script/PauseMenu.cs
+++ Assets/Script/PauseMenu.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;
 
 public class PausedMenuManager : MonoBehaviour
 {
@@ -31,6 +31,10 @@ public class PausedMenuManager : MonoBehaviour
         {
             Time.timeScale = 0;
         }
+    }
+
+    public void OpenScene5() {
+        SceneManager.LoadScene("SelectMap");
     }
     
 }
