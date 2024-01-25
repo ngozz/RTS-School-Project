2ec4feafc4e3e34773a280340cc4a0e88629c698
Hoanguyen-2002 <viethoah928@gmail.com>
2024-01-26 02:38:07 +0700

    update pause

--- Assets/Script/MapSelection.cs
+++ Assets/Script/MapSelection.cs
@@ -12,17 +12,21 @@ public class MapSelection : MonoBehaviour
 
     public void OpenScene() {
         SceneManager.LoadScene("DuyScene");
+        Time.timeScale = 1f;
     }
 
     public void OpenScene1() {
         SceneManager.LoadScene("HoaScene-2");
+        Time.timeScale = 1f;
     }
 
     public void OpenScene2() {
         SceneManager.LoadScene("HieuScene 2");
+        Time.timeScale = 1f;
     }
 
     public void OpenScene4() {
         SceneManager.LoadScene("Main Menu");
+        Time.timeScale = 1f;
     }
 }
